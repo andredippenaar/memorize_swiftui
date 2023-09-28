7e1b822b577bc13360fa5d2869f6062277e82277
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2023-09-28 15:33:56 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andre Dippenaar on 2023/09/28.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
